78ccdfdca3c151326427535d7c2c34bbc5ab654b
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-29 15:55:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 file=/opt/dist/deployed.done
 if [ ! -f $file ] ; then
   
-  mkdir /var/www/html/${INSTANCENAME}
-  cp -R /var/www/html/* /var/www/html/${INSTANCENAME}/
+  mkdir /var/www/html/nextcloud2028
+  cp -R /var/www/html/* /var/www/html/nextcloud2028/
   
-  rm -R /var/www/html/nextcloud2027/${INSTANCENAME}/
+  rm -R /var/www/html/nextcloud2027/nextcloud2028/
   
-  chown -R www-data /var/www/html/${INSTANCENAME}/
+  chown -R www-data /var/www/html/nextcloud2028/
   
   service apache2 reload
   apt-get update
