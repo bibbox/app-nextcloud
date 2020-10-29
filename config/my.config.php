41d021f6486030781a98ec47682ab0c0084631bf
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-29 15:14:43 +0100

    Update my.config.php

--- config/my.config.php
+++ config/my.config.php
@@ -3,5 +3,5 @@ $CONFIG = array (
   "trusted_proxies"   => ['127.0.0.1:8010'],
   "overwritehost"     => "localhost:8010",
   "overwriteprotocol" => "http",
-  "overwritewebroot"  => "/nextcloud2023",
+  "overwritewebroot"  => "/nextcloud2024",
 );
