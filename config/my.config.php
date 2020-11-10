5fd6ecda94b3628d598efba3adec99aebf0741b1
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-11-10 14:49:03 +0100

    Add files via upload

---
+++ config/my.config.php
@@ -0,0 +1,8 @@
+<?php
+$CONFIG = array (
+  "trusted_proxies"   => ['127.0.0.1:8010'],
+  "overwritehost"     => "localhost:8010",
+  "overwriteprotocol" => "http",
+  "overwritewebroot"  => "/§§INSTANCENAME",
+  "htaccess.RewriteBase" => "/§§INSTANCENAME",
+);
