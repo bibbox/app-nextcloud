3c4eb51885de8eadf062a7e2d667effeedebaef2
root <root@silicolab.bibbox>
2020-11-10 14:32:44 +0100

    change entrypoint permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ file=/opt/dist/deployed.done
 if [ ! -f $file ] ; then
   
   mkdir /var/www/html/nextcloud2028
-  cp -R /var/www/html/* /var/www/html/§§INSTANCENAME/
+  cp -R /var/www/html/* /var/www/html/next8/
   
-  rm -R /var/www/html/nextcloud2027/§§INSTANCENAME/
+  rm -R /var/www/html/nextcloud2027/next8/
   
-  chown -R www-data /var/www/html/§§INSTANCENAME/
+  chown -R www-data /var/www/html/next8/
   
   service apache2 reload
   apt-get update
