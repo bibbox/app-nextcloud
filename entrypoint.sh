0c50b0fd5a588f8915c6326c33a80e3019894a2a
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-11-10 15:18:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 file=/opt/dist/deployed.done
 if [ ! -f $file ] ; then
-  if pidof apache2 > /dev/null ; then
-    mkdir /var/www/html/§§INSTANCENAME
-    cp -R /var/www/html/* /var/www/html/§§INSTANCENAME/
+  while [ ! pidof apache2 > /dev/null ]; do sleep 1; done
+  mkdir /var/www/html/§§INSTANCENAME
+  cp -R /var/www/html/* /var/www/html/§§INSTANCENAME/
 
-    rm -R /var/www/html/§§INSTANCENAME/§§INSTANCENAME/
+  rm -R /var/www/html/§§INSTANCENAME/§§INSTANCENAME/
 
-    chown -R www-data /var/www/html/§§INSTANCENAME/
+  chown -R www-data /var/www/html/§§INSTANCENAME/
 
-    apt-get update
-    apt-get install nano
+  apt-get update
+  apt-get install nano
 
-    service apache2 reload
-    mkdir /opt/dist/
-    touch /opt/dist/deployed.done
-    #service apache2 start
-    #tail -f /var/log/dpkg.log
-  fi  
+  service apache2 reload
+  mkdir /opt/dist/
+  touch /opt/dist/deployed.done
+  #service apache2 start
+  #tail -f /var/log/dpkg.log  
 fi
