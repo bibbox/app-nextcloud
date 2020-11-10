fad514ef810ae101e5862ea072891d43ee1f8489
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-11-10 14:25:55 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+file=/opt/dist/deployed.done
+if [ ! -f $file ] ; then
+  
+  mkdir /var/www/html/nextcloud2028
+  cp -R /var/www/html/* /var/www/html/§§INSTANCENAME/
+  
+  rm -R /var/www/html/nextcloud2027/§§INSTANCENAME/
+  
+  chown -R www-data /var/www/html/§§INSTANCENAME/
+  
+  service apache2 reload
+  apt-get update
+  apt-get install nano
+  
+  
+  mkdir /opt/dist/
+  touch /opt/dist/deployed.done
+  service apache2 start
+  tail -f /var/log/dpkg.log
+fi
